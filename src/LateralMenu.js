58ea9ecf8ccde7f76a4bb6625ecd634c15cb2e50
Ch <carlo.martinucci@gmail.com>
2016-11-19 01:31:05 +0100

    init stato-regioni

--- src/LateralMenu.js
+++ src/LateralMenu.js
@@ -25,14 +25,41 @@ export default class LateralMenu extends React.Component {
             primaryTogglesNestedList
             open
             nestedItems={[
-              <MenuItem containerElement={<NavLink to="/referendum/senato/ruolo_e_funzioni" />} key="ruolo_e_funzioni" primaryText="Ruolo e funzioni" />,
-              <MenuItem containerElement={<NavLink to="/referendum/senato/modalità_di_elezione" />} key="modalità_di_elezione" primaryText="Modalità di elezione" />,
-              <MenuItem disabled key="il_procedimento_legislativo" primaryText="Il procedimento legislativo" />,
-              <MenuItem disabled key="i_diritti_dei_senatori" primaryText="I diritti dei senatori" />
+              <MenuItem
+                containerElement={<NavLink to="/referendum/senato/ruolo_e_funzioni" />}
+                key="ruolo_e_funzioni"
+                primaryText="Ruolo e funzioni"
+              />,
+              <MenuItem
+                containerElement={<NavLink to="/referendum/senato/modalità_di_elezione" />}
+                key="modalità_di_elezione"
+                primaryText="Modalità di elezione"
+              />,
+              <MenuItem
+                containerElement={<NavLink to="/referendum/senato/il_procedimento_legislativo" />}
+                key="il_procedimento_legislativo"
+                primaryText="Il procedimento legislativo"
+              />,
+              <MenuItem
+                containerElement={<NavLink to="/referendum/senato/diritti_dei_senatori_e_statuto_delle_minoranze" />}
+                key="diritti_dei_senatori_e_statuto_delle_minoranze"
+                primaryText="I diritti dei senatori"
+              />
             ]}
           />
           <Divider />
-          <MenuItem key="stato_regioni" disabled={true}>Rapporto Stato - Regioni</MenuItem>
+          <ListItem
+            key="stato-regioni"
+            primaryText="Rapporto Stato - Regioni"
+            primaryTogglesNestedList
+            nestedItems={[
+              <MenuItem
+                containerElement={<NavLink to="/referendum/stato-regioni/il_procedimento_legislativo" />}
+                key="il_procedimento_legislativo"
+                primaryText="Il procedimento legislativo"
+              />
+            ]}
+          />
           <Divider />
           <MenuItem key="resto" disabled={true}>Il resto</MenuItem>
           <Divider />
